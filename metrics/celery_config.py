c497e5e8c18ac8735a665921b7bf3167e4ddbc26
ixorlive <igorshakirov43@gmail.com>
2024-03-16 12:42:44 +0300

    remove some useless files

--- metrics/celery_config.py
+++
@@ -1,5 +0,0 @@
-BROKER_URL = "pyamqp://gpn:gpn@localhost:5672//"
-CELERY_RESULT_BACKEND = "rpc://"
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_ACCEPT_CONTENT = ["json"]
